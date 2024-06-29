d229e11a996c659c6ee1e7b7a8498fb4bf57db5e
rohit0828 <157058499+rohit0828@users.noreply.github.com>
2024-06-29 19:46:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var Typed= new Typed(".text",{
-    strings:["Frontend Developer","Competitive Programmer","Web Developer","Tech enthusiast"],
+    strings:["Frontend Developer","Competitive Programmer","Web Developer","Tech Enthusiast"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
