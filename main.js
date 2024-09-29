31fc5f99fc1559911365d84a953e011b67703b6d
rohit0828 <157058499+rohit0828@users.noreply.github.com>
2024-09-29 19:48:53 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var Typed= new Typed(".text",{
-    strings:["Frontend Developer","Competitive Programmer","Web Developer","Tech Enthusiast"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-});
